451dbc5a7d68c75ddaa759e31bf685e08a67b719
tenzin2017 <tenzinleo@gmail.com>
2017-08-24 13:54:06 -0400

    crm web part 1 is done

--- crm.rb
+++ crm.rb
@@ -2,5 +2,23 @@ require_relative 'contact'
 require 'sinatra'
 
 get '/' do
-'Welcome to my CRM'
+  redirect to('/index')
+end
+get '/index' do
+    erb :index
+end
+
+get '/contacts' do
+  @title = 'My Contact list'
+   @contacts = Contact.all
+   erb :contacts
+end
+
+get '/about' do
+  erb  :about
+end
+
+
+after do
+  ActiveRecord::Base.connection.close
 end
